8730361d12a6de7935d93a4def439bdfb916090e
Rodrigo Chi <chirodrigo497@gamil.com>
2026-02-25 19:45:50 -0600

    chore: install spatie/laravel-permission, datatables and wireui

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -17,6 +17,8 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
         <script src="https://kit.fontawesome.com/a261298d21.js" crossorigin="anonymous"></script>
+        {{-- wireUI --}}
+        <wireui:scripts />
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
